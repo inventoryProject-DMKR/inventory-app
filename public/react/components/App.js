a5c542c3a6883112ec6a007178dd4ccbeb36fed7
rodrigosollberger <120380147+rodrigosollberger@users.noreply.github.com>
2023-03-08 14:24:57 +0000

    Update App.js

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
+import { ItemsList } from './ItemsList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -7,6 +8,7 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [sauces, setSauces] = useState([]);
+	const [items, setItems] = useState([]);
 
 	async function fetchSauces(){
 		try {
@@ -19,15 +21,32 @@ export const App = () => {
 		}
 	}
 
+
+	async function fetchItems(){
+		try {
+			const itemsResponse = await fetch(`${apiURL}/items`);
+			const itemsData = await itemsResponse.json();
+			
+			setItems(itemsData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
+
 	useEffect(() => {
 		fetchSauces();
+		fetchItems();
 	}, []);
 
 	return (
-		<main>	
-      <h1>Sauce Store</h1>
+		<main>
+			<h1>Sauce Store</h1>
 			<h2>All things 🔥</h2>
 			<SaucesList sauces={sauces} />
+			<h1>Item Store</h1>
+			<h2>All items </h2>
+			<ItemsList items={items} />
 		</main>
 	)
 }
