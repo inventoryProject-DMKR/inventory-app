f40c27c687749ef89fd835e79c19359d22b99f24
moneshapb <120640597+moneshapb@users.noreply.github.com>
2023-03-10 11:56:35 +0000

    Update IndividualItem.js

--- public/react/components/IndividualItem.js
+++ public/react/components/IndividualItem.js
@@ -3,10 +3,10 @@ import React from 'react';
 export function IndividualItem (props) {
     return (
       <>
-        <h3>{props.item.title}</h3>
-        <h4> Price: £{props.item.price}</h4>
-        <h4>Item Description: {props.item.description}</h4>
-        <h4>Item Category: {props.item.category}</h4>
+        <h3 id="items">{props.item.title}</h3>
+        <h4 id="items"> Price: £{props.item.price}</h4>
+        <h4 id="items">Item Description: {props.item.description}</h4>
+        <h4 id"items">Item Category: {props.item.category}</h4>
         <img id="image" src={props.item.image} alt={props.item.title} />
         <button id="button" onClick={() => props.setIndividualItem()}>Back to Wiki List</button>
       </>
