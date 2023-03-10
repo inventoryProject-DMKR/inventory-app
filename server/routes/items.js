13c4a5ab8b28bfce0f5bd791d94c14d0ac12dcaa
rodrigosollberger <120380147+rodrigosollberger@users.noreply.github.com>
2023-03-10 11:27:47 +0000

    final commit

--- server/routes/items.js
+++ server/routes/items.js
@@ -28,6 +28,45 @@ router.get('/:id', async (req, res, next) => {
 });
 
 
+/  create /post
+//*****works*****
+const { body, validationResult } = require('express-validator');//gives us the option to validate input
+// think this path may need updated
+router.post("/",
+//checks if title, price, description, category and image are NOT empty
+//may be a less repetitive way of doing this...? body ('title', 'price' etcccc)
+body('title').not().isEmpty(),
+body('price').not().isEmpty(),
+body('description').not().isEmpty(),
+body('category').not().isEmpty(),
+body('image').not().isEmpty(),
+async (req, res) => {
+  //check for erros nd return status
+const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const newItem = await Item.create(req.body);
+      res.status(200).send({ msg: "Success", newItem });
+    } catch (error) {
+      res.status(500).send({ err: error.message });
+    }
+  });
 
+
+// Delete works - but isnt perfect - give successfull message when nothing to delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedItem = Item.destroy({ where: { id: req.params.id } });
+    // if (deletedItem) {
+      res.status(200).send({ msg: "Success", deletedItem });
+    // } else {
+    //   throw new Error("Not deleted");
+    // }
+  } catch (error) {
+    res.status(500).send({ err: error.message });
+    
+    
 
 module.exports = router;
